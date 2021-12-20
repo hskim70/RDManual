021c4820c0bb15b5259636064ddc9720cc916d04
hskim70 <hminky@gmail.com>
2021-12-20 14:59:41 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RDManual'
+copyright = '2021, FunctionBay'
+author = 'Hyun Soo Kim'
 
 release = '0.1'
 version = '0.1.0'
